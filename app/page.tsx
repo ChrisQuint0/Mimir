cbc25eb16cd45922f780162befa438d19c7936b3
Christopher Quinto <cquinto.primary@gmail.com>
2026-01-25 21:58:20 +0800

    Initial project setup with Next.js and UI components
    Sets up a new project for Mimir
    
    Signed-off-by: Christopher Quinto <cquinto.primary@gmail.com>

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="p-8">
+      <Button className="hover: opacity-90 hover:cursor-pointer">
+        Begin Your Journey
+      </Button>
+    </main>
+  );
+}
